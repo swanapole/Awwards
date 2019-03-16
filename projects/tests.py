c23131ed9d3ed13b6070e5bf1b50feca88fba673
swanapole <swanapoleshilla@yahoo.com>
2019-03-16 15:36:21 +0300

    Added tests to my readme

--- projects/tests.py
+++ projects/tests.py
@@ -2,3 +2,13 @@ from django.test import TestCase
 from .models import categories,technologies,colors,countries
 from django.contrib.auth.models import User
 import datetime as dt
+# Create your tests here.
+class categoriesTestClass(TestCase):
+    def setUp(self):
+        self.Art = categories(categories='Art')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Art,categories))
+
+    def tearDown(self):
+        categories.objects.all().delete()
