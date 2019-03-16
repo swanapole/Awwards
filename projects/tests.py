a426ef101284e3500ed0dc3b760941c536a13861
swanapole <swanapoleshilla@yahoo.com>
2019-03-16 15:46:10 +0300

    Added tests

--- projects/tests.py
+++ projects/tests.py
@@ -50,3 +50,11 @@ class countriesTestClass(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.Kenya,countries))
+
+    def tearDown(self):
+        countries.objects.all().delete()
+
+    def test_save_method(self):
+        self.Kenya.save_country()
+        country = countries.objects.all()
+        self.assertTrue(len(country)>0)
